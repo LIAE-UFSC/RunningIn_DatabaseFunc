f523d61e0e2637f554d7bafadfcd0fbe9d2a685b
ThalerG <thalergabriel@gmail.com>
2024-10-02 12:43:07 -0300

    Fix test

--- tests/getMeasurments.py
+++ tests/getMeasurments.py
@@ -1,10 +1,10 @@
 import pyRInDB as ridb
 
-pathIn = r"\\LIAE-SANTINHO\Backups\Amaciamento_DatabaseFull\datasetModelA.hdf5"
+pathIn = r"\\LIAE-SANTINHO\Backups\Amaciamento_DatabaseFull\ModelA.hdf5"
 
 file =  ridb.RunIn_File(pathIn)
 
-df = file.to_dataframe(vars = ["time","massFlow","current"],
+df = file.to_dataframe(vars = ["time","massFlow","current","power"],
                        processesdVars = [
                             {"var":"current","process":["Kurtosis","RMS"]},
                             {"var":"vibrationLateral","process":["Variance"]}],
