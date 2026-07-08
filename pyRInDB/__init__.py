6684b23753074402bacdef18548429e9225de260
Pedro Jann Luna <pedrojluna26@gmail.com>
2026-07-08 11:27:09 +0200

    chore: remove pyRInDB library and packaging

--- pyRInDB/__init__.py
+++
@@ -1,1 +0,0 @@
-from .main_class import RunIn_File
--- tests/raw2hdf.py
+++
@@ -1,9 +0,0 @@
-# Call functions from root folder
-import sys, os
-sys.path.append(os.path.abspath('.'))
-
-import pyRInDB as ridb
-
-folderIn = r"\\LIAE-SANTINHO\Backups\Amaciamento_DadosBrutos"
-folderOut = r"\\LIAE-SANTINHO\Backups\Amaciamento_DatabaseFull"
-ridb.RunIn_File.convertFolders(folderIn, folderOut, supressWarnings = False)
